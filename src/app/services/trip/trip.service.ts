5875a64012199db53ec80aa779eee6f4ace5c87d
issamunic <89040136+issamunic@users.noreply.github.com>
2022-04-23 17:14:53 +0100

    lol

---
+++ src/app/services/trip/trip.service.ts
@@ -0,0 +1,29 @@
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class TripService {
+
+  url="http://localhost:8087";
+
+  constructor(private http : HttpClient) { }
+
+  getTrips(){
+    console.log("appel trip");
+    
+   
+      const headers = new HttpHeaders({
+        'Content-Type': 'application/json',
+        'Authorization': "Bearer "+localStorage.getItem('token')
+      })
+      console.log(headers);
+      
+    return this.http.get(this.url+"/SpringMVC/trip/getAll", { headers: headers });
+  }
+
+
+
+}
