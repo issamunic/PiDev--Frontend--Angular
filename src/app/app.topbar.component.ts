1a45aa7ca48b97b41d98c0e41780a620162e7982
mohamedbenrabah <86411808+mohamedbenrabah@users.noreply.github.com>
2022-05-01 16:44:45 +0100

    app-topbar-componant update

--- src/app/app.topbar.component.ts
+++ src/app/app.topbar.component.ts
@@ -2,14 +2,30 @@ import { Component, OnDestroy } from '@angular/core';
 import { AppMainComponent } from './app.main.component';
 import { Subscription } from 'rxjs';
 import { MenuItem } from 'primeng/api';
+import { UserAuthService } from './services/user-auth/user-auth.service';
+import { Router } from '@angular/router';
+import { UserService } from './services/user/user.service';
 
 @Component({
     selector: 'app-topbar',
     templateUrl: './app.topbar.component.html'
 })
 export class AppTopBarComponent {
-
+    
     items: MenuItem[];
 
-    constructor(public appMain: AppMainComponent) { }
+    constructor(public appMain: AppMainComponent,private userAuthService:UserAuthService,
+        private router:Router,
+        public userService:UserService) 
+        { }
+
+    public isLoggedIn(){
+        return this.userAuthService.isLoggedIn();
+    }
+
+    public logOut(){
+        this.userAuthService.clear();
+        this.router.navigate(['/home']);
+    }    
+    
 }
