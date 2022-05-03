1a817576e649f0e8c16dcd14fa2c1d9a69bcaa21
mohamedbenrabah <86411808+mohamedbenrabah@users.noreply.github.com>
2022-05-03 22:53:05 +0100

    register template

--- src/app/register/register.component.ts
+++ src/app/register/register.component.ts
@@ -1,51 +1,85 @@
-import { Component,OnInit,ViewEncapsulation } from '@angular/core';
-import {MenuItem} from 'primeng/api';
-import {MessageService} from 'primeng/api';
+import { Component, OnInit, ViewEncapsulation } from '@angular/core';
+import { MenuItem } from 'primeng/api';
+import { MessageService } from 'primeng/api';
 
-@Component({
+/*@Component({
   selector: 'app-register',
   templateUrl: './register.component.html',
   styleUrls: ['./register.component.scss']
+})*/
+@Component({
+  selector: 'app-register',
+  templateUrl: './register.component.html',
+  providers: [MessageService],
+  styles: [`
+        .ui-steps .ui-steps-item {
+            width: 25%;
+        }
+        
+        .ui-steps.steps-custom {
+            margin-bottom: 30px;
+        }
+        
+        .ui-steps.steps-custom .ui-steps-item .ui-menuitem-link {
+            padding: 0 1em;
+            overflow: visible;
+        }
+        
+        .ui-steps.steps-custom .ui-steps-item .ui-steps-number {
+            background-color: #0081c2;
+            color: #FFFFFF;
+            display: inline-block;
+            width: 36px;
+            border-radius: 50%;
+            margin-top: -14px;
+            margin-bottom: 10px;
+        }
+        
+        .ui-steps.steps-custom .ui-steps-item .ui-steps-title {
+            color: #555555;
+        }
+    `],
+  encapsulation: ViewEncapsulation.None
 })
 export class RegisterComponent implements OnInit {
 
-  
-  items: MenuItem[];
-    
-    activeIndex: number = 1;
-    
-    constructor(private messageService: MessageService) {}
 
-    ngOnInit() {
-        this.items = [{
-                label: 'Personal',
-                command: (event: any) => {
-                    this.activeIndex = 0;
-                    this.messageService.add({severity:'info', summary:'First Step', detail: event.item.label});
-                }
-            },
-            {
-                label: 'Seat',
-                command: (event: any) => {
-                    this.activeIndex = 1;
-                    this.messageService.add({severity:'info', summary:'Seat Selection', detail: event.item.label});
-                }
-            },
-            {
-                label: 'Payment',
-                command: (event: any) => {
-                    this.activeIndex = 2;
-                    this.messageService.add({severity:'info', summary:'Pay with CC', detail: event.item.label});
-                }
-            },
-            {
-                label: 'Confirmation',
-                command: (event: any) => {
-                    this.activeIndex = 3;
-                    this.messageService.add({severity:'info', summary:'Last Step', detail: event.item.label});
-                }
-            }
-        ];
-    }    
+  items: MenuItem[];
+
+  activeIndex: number = 1;
+
+  constructor(private messageService: MessageService) { }
+
+  ngOnInit() {
+    this.items = [{
+      label: 'Personal',
+      command: (event: any) => {
+        this.activeIndex = 0;
+        this.messageService.add({ severity: 'info', summary: 'First Step', detail: event.item.label });
+      }
+    },
+    {
+      label: 'Seat',
+      command: (event: any) => {
+        this.activeIndex = 1;
+        this.messageService.add({ severity: 'info', summary: 'Seat Selection', detail: event.item.label });
+      }
+    },
+    {
+      label: 'Payment',
+      command: (event: any) => {
+        this.activeIndex = 2;
+        this.messageService.add({ severity: 'info', summary: 'Pay with CC', detail: event.item.label });
+      }
+    },
+    {
+      label: 'Confirmation',
+      command: (event: any) => {
+        this.activeIndex = 3;
+        this.messageService.add({ severity: 'info', summary: 'Last Step', detail: event.item.label });
+      }
+    }
+    ];
+  }
 
 }
